845429b050cedf73be52a2907637ec1f59be88ec
Indra Bagus Wicaksono <sonny8988@gmail.com>
2014-11-09 09:21:16 +0900

    Initial Commit

---
+++ stepHack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  stepHack
+//
+//  Created by Indra Bagus Wicaksono on 11/9/14.
+//  Copyright (c) 2014 Indra Bagus Wicaksono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ stepHack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stepHack
+//
+//  Created by Indra Bagus Wicaksono on 11/9/14.
+//  Copyright (c) 2014 Indra Bagus Wicaksono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
